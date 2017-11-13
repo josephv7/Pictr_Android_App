c9d255ba6668790a41da6f6c4c24d85087d3c072
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-11-13 23:27:06 +0530

    added glide dependency....displayAdapter almost done

--- app/src/main/java/rm/com/microproject/DisplayAdapter.java
+++ app/src/main/java/rm/com/microproject/DisplayAdapter.java
@@ -2,7 +2,9 @@ package rm.com.microproject;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -34,10 +36,36 @@ public class DisplayAdapter extends RecyclerView.Adapter<DisplayAdapter.ViewHold
         public ViewHolder(View itemView) {
             super(itemView);
             layout = itemView;
+
+
+            imageView = itemView.findViewById(R.id.imageView);
+            imageId = itemView.findViewById(R.id.imageId);
         }
     }
 
 
+    @Override
+    public DisplayAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
 
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        View v = inflater.inflate(R.layout.list_item,parent,false);
+        ViewHolder vh = new ViewHolder(v);
+        return  vh;
+
+
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position, List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+
+        ImageDetails data = imageDetails.get(position);
+
+        holder.imageId.setText(data.getImageId());
+
+    }
 }
