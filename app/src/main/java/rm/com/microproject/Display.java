f5c0d0ec2475f03b39c397d6e0392e0762d6a80e
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-11-13 22:33:03 +0530

    starting with display activity...added recyclerview and necessary dependency

--- app/src/main/java/rm/com/microproject/Display.java
+++ app/src/main/java/rm/com/microproject/Display.java
@@ -1,13 +1,42 @@
 package rm.com.microproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Display extends AppCompatActivity {
+
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    List<ImageDetails> imageList;
+    DBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+
+        dbHandler = new DBHandler(getApplicationContext());
+        imageList = new ArrayList<ImageDetails>();
+
+
+        imageList = dbHandler.getAllImages();
+
+
+
+
+
     }
 }
