0bf5b2d88332e3960d558b31be3f1f353eb60949
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-11-21 11:34:48 +0530

    dialog added in Home but inside onCreate....locations needs to be changed

--- app/src/main/java/rm/com/microproject/Home.java
+++ app/src/main/java/rm/com/microproject/Home.java
@@ -3,9 +3,14 @@ package rm.com.microproject;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 
 public class Home extends AppCompatActivity {
 
@@ -15,6 +20,9 @@ public class Home extends AppCompatActivity {
     DBHandler dbHandler;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+
+
+    MaterialDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,23 @@ public class Home extends AppCompatActivity {
 
         sharedPreferences = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
         editor = sharedPreferences.edit();
+
+        dialog = new MaterialDialog.Builder(Home.this)
+                .title("Enter You Choice")
+                .content("Please Wait")
+                .positiveText("Storage")
+                .negativeText("Camera")
+                .progress(true, 0)
+                .progressIndeterminateStyle(true)
+                .cancelable(false)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        Log.d("positive clicked","............");
+                    }
+                })
+                .show();
+
 
 
 
